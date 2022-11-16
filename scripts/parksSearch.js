36009dace3d156811df6784755538994d02ebb66
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-15 21:03:29 -0500

    create function to handel onclick event for 2 button which can hide and show element

--- scripts/parksSearch.js
+++ scripts/parksSearch.js
@@ -1,4 +1,9 @@
 import {nationalParksArray} from "./nationalParkData.js";
 
 
-let parkType = document.ge
+// let parkType = document.getElementById("parkType");
+// console.log(parkType);
+// let parkLocation = document.getElementById("parkLocation");
+// console.log(parkLocation);
+// let showAllBtn = document.getElementById("showAll");
+// console.log(showAllBtn);
