e5b014a95c21e66591f804c3fd04df0d5b104994
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-16 22:47:41 -0500

    create getParksByLocation function where user can search by state with onchange method it is working but i have to refresh a page everytime

--- scripts/parksSearch.js
+++ scripts/parksSearch.js
@@ -4,11 +4,14 @@ import { parkTypesArray } from "./parkTypeData.js";
 
 ///getting all html element to manipulate
 const showAllBtn = document.getElementById("showAll");
+
 const parkTypeBtn = document.getElementById("parkType");
 const sParkTypeBtn = document.getElementById("sParkType");
+
 const parkLocationBtn = document.getElementById("parkLocation");
 const sParkLocationBtn = document.getElementById("sLocation");
-const tBodyEl = document.getElementById('tBody');
+
+const tBodyEl = document.getElementById("tBody");
 
 ///this is just hide and show button as user click
 sParkTypeBtn.style.display = "none";
@@ -29,84 +32,138 @@ parkLocationBtn.onclick = function () {
   }
 };
 
-
-///to put option for  choose  by location dropdown 
+///to put option for  choose  by location dropdown
 locationsArray.forEach(function (av) {
-    let optionElement = new Option(av);
-    sLocation.appendChild(optionElement);
-})
+  let optionElement = new Option(av);
+  sLocation.appendChild(optionElement);
+});
 
-///to put option for  choose  by park type dropdown 
-parkTypesArray.forEach(function(av) {
-    let optionElement = new Option(av);
-    sParkTypeBtn.appendChild(optionElement);
-
+///to put option for  choose  by park type dropdown
+parkTypesArray.forEach(function (av) {
+  let optionElement = new Option(av);
+  sParkTypeBtn.appendChild(optionElement);
 });
 /// to show all park when user click show all parks button
-showAllBtn.onclick = filterData;
+showAllBtn.onclick = getAllData;
 
-function filterData() {
-    console.log('it works');
+function getAllData() {
+  nationalParksArray.forEach((av) => {
+    let tr = document.createElement("tr");
 
-    nationalParksArray.forEach(av => {
-        let tr = document.createElement("tr");
-        // tBodyEl.appendChild(tr);
+    let td1 = document.createElement("td");
+    td1.innerText = av.LocationID;
+    tr.appendChild(td1);
 
-        let td1 = document.createElement("td");
-        td1.innerText = av.LocationID;
-        tr.appendChild(td1);
+    let td2 = document.createElement("td");
+    td2.innerText = av.LocationName;
+    tr.appendChild(td2);
 
-        let td2 = document.createElement("td");
-        td2.innerText = av.LocationName;
-        tr.appendChild(td2);
+    let td3 = document.createElement("td");
+    td3.innerText = av.Address;
+    tr.appendChild(td3);
 
-        let td3 = document.createElement("td");
-        td3.innerText = av.Address;
-        tr.appendChild(td3);
+    let td4 = document.createElement("td");
+    td4.innerText = av.City;
+    tr.appendChild(td4);
 
-        let td4 = document.createElement("td");
-        td4.innerText = av.City;
-        tr.appendChild(td4);
+    let td5 = document.createElement("td");
+    td5.innerText = av.State;
+    tr.appendChild(td5);
 
-        let td5 = document.createElement("td");
-        td5.innerText = av.State;
-        tr.appendChild(td5);
+    let td6 = document.createElement("td");
+    td6.innerText = av.ZipCode;
+    tr.appendChild(td6);
 
-        let td6 = document.createElement("td");
-        td6.innerText = av.ZipCode;
-        tr.appendChild(td6);
+    let td7 = document.createElement("td");
+    td7.innerText = av.Phone;
+    tr.appendChild(td7);
 
-        let td7 = document.createElement("td");
-        td7.innerText = av.Phone;
-        tr.appendChild(td7);
+    let td8 = document.createElement("td");
+    td8.innerText = av.Fax;
+    tr.appendChild(td8);
 
-        let td8 = document.createElement("td");
-        td8.innerText = av.Fax;
-        tr.appendChild(td8);
+    let td9 = document.createElement("td");
+    td9.textContent = av.Latitude;
+    tr.appendChild(td9);
 
-        let td9 = document.createElement("td");
-        td9.textContent = av.Latitude;
-        tr.appendChild(td9);
+    let td10 = document.createElement("td");
+    td10.textContent = av.Longitude;
+    tr.appendChild(td10);
 
-        let td10 = document.createElement("td");
-        td10.textContent = av.Longitude;
-        tr.appendChild(td10);
+    let td11 = document.createElement("td");
+    td11.textContent = `${av.Location.coordinates} ${av.Location.type}`;
+    tr.appendChild(td11);
 
-        let td11 = document.createElement("td");
-        td11.textContent = `${av.Location.coordinates} ${av.Location.type}`;
-        tr.appendChild(td11);
-        
-        let td13 = document.createElement("td");
-        td13.textContent = av.Visit;
-        tr.appendChild(td13);
-        
-        tBodyEl.appendChild(tr);
-    });
+    let td13 = document.createElement("td");
+    td13.textContent = av.Visit;
+    tr.appendChild(td13);
+
+    tBodyEl.appendChild(tr);
+  });
 }
 
+//show parks by location
 
+sParkLocationBtn.onchange = getParksByLocation;
 
+function getParksByLocation() {
+  console.log("function works");
+  console.log(sParkLocationBtn.value);
+  // let selectedPark =  nationalParksArray.filter(av =>  sParkLocationBtn.value == av.State );
+  nationalParksArray.forEach((av) => {
+    // console.log(av.State);
+    if (sParkLocationBtn.value == av.State) {
+      let tr = document.createElement("tr");
 
+      let td1 = document.createElement("td");
+      td1.innerText = av.LocationID;
+      tr.appendChild(td1);
 
+      let td2 = document.createElement("td");
+      td2.innerText = av.LocationName;
+      tr.appendChild(td2);
 
+      let td3 = document.createElement("td");
+      td3.innerText = av.Address;
+      tr.appendChild(td3);
 
+      let td4 = document.createElement("td");
+      td4.innerText = av.City;
+      tr.appendChild(td4);
+
+      let td5 = document.createElement("td");
+      td5.innerText = av.State;
+      tr.appendChild(td5);
+
+      let td6 = document.createElement("td");
+      td6.innerText = av.ZipCode;
+      tr.appendChild(td6);
+
+      let td7 = document.createElement("td");
+      td7.innerText = av.Phone;
+      tr.appendChild(td7);
+
+      let td8 = document.createElement("td");
+      td8.innerText = av.Fax;
+      tr.appendChild(td8);
+
+      let td9 = document.createElement("td");
+      td9.textContent = av.Latitude;
+      tr.appendChild(td9);
+
+      let td10 = document.createElement("td");
+      td10.textContent = av.Longitude;
+      tr.appendChild(td10);
+
+      let td11 = document.createElement("td");
+      td11.textContent = `${av.Location.coordinates} ${av.Location.type}`;
+      tr.appendChild(td11);
+
+      let td13 = document.createElement("td");
+      td13.textContent = av.Visit;
+      tr.appendChild(td13);
+
+      tBodyEl.appendChild(tr);
+    }
+  });
+}
