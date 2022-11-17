7b6e7012e3f2de5695f06c8c913bc9eceafd9774
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-17 15:14:25 -0500

    create function which clear table when user click anywhere on the feildset

--- scripts/parksSearch.js
+++ scripts/parksSearch.js
@@ -10,32 +10,41 @@ const sParkTypeBtn = document.getElementById("sParkType");
 
 const parkLocationBtn = document.getElementById("parkLocation");
 const sParkLocationBtn = document.getElementById("sLocation");
-const feildSet = document.getElementById('feildSet');
+const feildSet = document.getElementById("feildSet");
+const feildSetSearch = document.getElementById("feildSetSearch");
+
 
 const tBodyEl = document.getElementById("tBody");
 
 ///this is just hide and show button as user click
 sParkTypeBtn.style.display = "none";
 sParkLocationBtn.style.display = "none";
+feildSet.style.display = "none";
 
+
+// feildSetSearch.onclick = function (){
+//     feildSet.style.display = "none";
+// }
+//handel parksTypeBtn
 parkTypeBtn.onclick = function () {
-    sParkLocationBtn.style.display = "none";
+  sParkLocationBtn.style.display = "none";
   if (sParkTypeBtn.style.display !== "none") {
     sParkTypeBtn.style.display = "none";
   } else {
     sParkTypeBtn.style.display = "inline";
   }
 };
+//handel parklocationBtn
 parkLocationBtn.onclick = function () {
-    sParkTypeBtn.style.display = "none";
+  sParkTypeBtn.style.display = "none";
   if (sParkLocationBtn.style.display !== "none") {
     sParkLocationBtn.style.display = "none";
   } else {
     sParkLocationBtn.style.display = "inline";
   }
 };
-
-
+//handel showAllBtn
+showAllBtn.onclick
 
 ///to put option for  choose  by location dropdown
 locationsArray.forEach(function (av) {
@@ -48,203 +57,95 @@ parkTypesArray.forEach(function (av) {
   let optionElement = new Option(av);
   sParkTypeBtn.appendChild(optionElement);
 });
-/// to show all park when user click show all parks button
+
+/// to show all parks when 
 showAllBtn.onclick = getAllData;
 
 function getAllData() {
+    feildSet.style.display = "block";
     sParkTypeBtn.style.display = "none";
-    sParkLocationBtn.style.display = "none";
-
-    tBodyEl.innerHTML = "";
-
-  nationalParksArray.forEach((av) => {
-    let tr = document.createElement("tr");
-
-    let td1 = document.createElement("td");
-    td1.innerText = av.LocationID;
-    tr.appendChild(td1);
-
-    let td2 = document.createElement("td");
-    td2.innerText = av.LocationName;
-    tr.appendChild(td2);
-
-    let td3 = document.createElement("td");
-    td3.innerText = av.Address;
-    tr.appendChild(td3);
-
-    let td4 = document.createElement("td");
-    td4.innerText = av.City;
-    tr.appendChild(td4);
-
-    let td5 = document.createElement("td");
-    td5.innerText = av.State;
-    tr.appendChild(td5);
-
-    let td6 = document.createElement("td");
-    td6.innerText = av.ZipCode;
-    tr.appendChild(td6);
-
-    let td7 = document.createElement("td");
-    td7.innerText = av.Phone;
-    tr.appendChild(td7);
-
-    let td8 = document.createElement("td");
-    td8.innerText = av.Fax;
-    tr.appendChild(td8);
-
-    let td9 = document.createElement("td");
-    td9.textContent = av.Latitude;
-    tr.appendChild(td9);
-
-    let td10 = document.createElement("td");
-    td10.textContent = av.Longitude;
-    tr.appendChild(td10);
-
-    let td11 = document.createElement("td");
-    td11.textContent = `${av.Location.coordinates} ${av.Location.type}`;
-    tr.appendChild(td11);
-
-    let td13 = document.createElement("td");
-    td13.textContent = av.Visit;
-    tr.appendChild(td13);
-
-    tBodyEl.appendChild(tr);
-  });
+  sParkLocationBtn.style.display = "none";
+  tBodyEl.innerHTML = "";
+  return nationalParksArray.forEach(array => getTableRowsAndColumns(array));
 }
 
 //show parks by location
-
 sParkLocationBtn.onchange = getParksByLocation;
 
 function getParksByLocation() {
-  console.log("function works");
-  console.log(sParkLocationBtn.value);
+    feildSet.style.display = "block";
   tBodyEl.innerHTML = "";
-
-  // let selectedPark =  nationalParksArray.filter(av =>  sParkLocationBtn.value == av.State );
-  nationalParksArray.forEach((av) => {
-    // console.log(av.State);
-    if (sParkLocationBtn.value == av.State) {
-      let tr = document.createElement("tr");
-
-      let td1 = document.createElement("td");
-      td1.innerText = av.LocationID;
-      tr.appendChild(td1);
-
-      let td2 = document.createElement("td");
-      td2.innerText = av.LocationName;
-      tr.appendChild(td2);
-
-      let td3 = document.createElement("td");
-      td3.innerText = av.Address;
-      tr.appendChild(td3);
-
-      let td4 = document.createElement("td");
-      td4.innerText = av.City;
-      tr.appendChild(td4);
-
-      let td5 = document.createElement("td");
-      td5.innerText = av.State;
-      tr.appendChild(td5);
-
-      let td6 = document.createElement("td");
-      td6.innerText = av.ZipCode;
-      tr.appendChild(td6);
-
-      let td7 = document.createElement("td");
-      td7.innerText = av.Phone;
-      tr.appendChild(td7);
-
-      let td8 = document.createElement("td");
-      td8.innerText = av.Fax;
-      tr.appendChild(td8);
-
-      let td9 = document.createElement("td");
-      td9.textContent = av.Latitude;
-      tr.appendChild(td9);
-
-      let td10 = document.createElement("td");
-      td10.textContent = av.Longitude;
-      tr.appendChild(td10);
-
-      let td11 = document.createElement("td");
-      td11.textContent = `${av.Location.coordinates} ${av.Location.type}`;
-      tr.appendChild(td11);
-
-      let td13 = document.createElement("td");
-      td13.textContent = av.Visit;
-      tr.appendChild(td13);
-
-      tBodyEl.appendChild(tr);
-    }
-  });
+  let selectedParkLoc = nationalParksArray.filter(
+    (av) => sParkLocationBtn.value == av.State
+  );
+  console.log(selectedParkLoc);
+  return selectedParkLoc.forEach((array) => getTableRowsAndColumns(array));
 }
 
-
 //show parks by park type
-
 sParkTypeBtn.onchange = getParksByType;
 
 function getParksByType() {
-  console.log("function works");
-  console.log(sParkTypeBtn.value);
+    feildSet.style.display = "block";
   tBodyEl.innerHTML = "";
+  let selectedParkType = nationalParksArray.filter((array) =>
+    array.LocationName.includes(sParkTypeBtn.value)
+  );
+  return selectedParkType.forEach((array) => getTableRowsAndColumns(array));
+}
 
-  // let selectedPark =  nationalParksArray.filter(av =>  sParkLocationBtn.value == av.State );
-  nationalParksArray.forEach((av) => {
-    if (av.LocationName.includes(sParkTypeBtn.value)) {
-        let tr = document.createElement("tr");
+///to create table when you pass argument
 
-      let td1 = document.createElement("td");
-      td1.innerText = av.LocationID;
-      tr.appendChild(td1);
+function getTableRowsAndColumns(array) {
+  let tr = document.createElement("tr");
 
-      let td2 = document.createElement("td");
-      td2.innerText = av.LocationName;
-      tr.appendChild(td2);
+  let td1 = document.createElement("td");
+  td1.innerText = array.LocationID;
+  tr.appendChild(td1);
 
-      let td3 = document.createElement("td");
-      td3.innerText = av.Address;
-      tr.appendChild(td3);
+  let td2 = document.createElement("td");
+  td2.innerText = array.LocationName;
+  tr.appendChild(td2);
 
-      let td4 = document.createElement("td");
-      td4.innerText = av.City;
-      tr.appendChild(td4);
+  let td3 = document.createElement("td");
+  td3.innerText = array.Address;
+  tr.appendChild(td3);
 
-      let td5 = document.createElement("td");
-      td5.innerText = av.State;
-      tr.appendChild(td5);
+  let td4 = document.createElement("td");
+  td4.innerText = array.City;
+  tr.appendChild(td4);
 
-      let td6 = document.createElement("td");
-      td6.innerText = av.ZipCode;
-      tr.appendChild(td6);
+  let td5 = document.createElement("td");
+  td5.innerText = array.State;
+  tr.appendChild(td5);
 
-      let td7 = document.createElement("td");
-      td7.innerText = av.Phone;
-      tr.appendChild(td7);
+  let td6 = document.createElement("td");
+  td6.innerText = array.ZipCode;
+  tr.appendChild(td6);
 
-      let td8 = document.createElement("td");
-      td8.innerText = av.Fax;
-      tr.appendChild(td8);
+  let td7 = document.createElement("td");
+  td7.innerText = array.Phone;
+  tr.appendChild(td7);
 
-      let td9 = document.createElement("td");
-      td9.textContent = av.Latitude;
-      tr.appendChild(td9);
+  let td8 = document.createElement("td");
+  td8.innerText = array.Fax;
+  tr.appendChild(td8);
 
-      let td10 = document.createElement("td");
-      td10.textContent = av.Longitude;
-      tr.appendChild(td10);
+  let td9 = document.createElement("td");
+  td9.textContent = array.Latitude;
+  tr.appendChild(td9);
 
-      let td11 = document.createElement("td");
-      td11.textContent = `${av.Location.coordinates} ${av.Location.type}`;
-      tr.appendChild(td11);
+  let td10 = document.createElement("td");
+  td10.textContent = array.Longitude;
+  tr.appendChild(td10);
 
-      let td13 = document.createElement("td");
-      td13.textContent = av.Visit;
-      tr.appendChild(td13);
+  let td11 = document.createElement("td");
+  td11.textContent = `${array.Location.coordinates} ${array.Location.type}`;
+  tr.appendChild(td11);
 
-      tBodyEl.appendChild(tr);
+  let td13 = document.createElement("td");
+  td13.textContent = array.Visit;
+  tr.appendChild(td13);
 
-    }
-  });
+  tBodyEl.appendChild(tr);
 }
