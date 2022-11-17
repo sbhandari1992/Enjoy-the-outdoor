990f5df5c878e1d2fab6110b2863f8248384f541
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-16 22:58:14 -0500

    create getParksByTypen function where user can search by park types  with onchange event it is working but i have to refresh a page everytime

--- scripts/parksSearch.js
+++ scripts/parksSearch.js
@@ -167,3 +167,70 @@ function getParksByLocation() {
     }
   });
 }
+
+
+//show parks by park type
+
+sParkTypeBtn.onchange = getParksByType;
+
+function getParksByType() {
+  console.log("function works");
+  console.log(sParkTypeBtn.value);
+  // let selectedPark =  nationalParksArray.filter(av =>  sParkLocationBtn.value == av.State );
+  nationalParksArray.forEach((av) => {
+    if (av.LocationName.includes(sParkTypeBtn.value)) {
+        let tr = document.createElement("tr");
+
+      let td1 = document.createElement("td");
+      td1.innerText = av.LocationID;
+      tr.appendChild(td1);
+
+      let td2 = document.createElement("td");
+      td2.innerText = av.LocationName;
+      tr.appendChild(td2);
+
+      let td3 = document.createElement("td");
+      td3.innerText = av.Address;
+      tr.appendChild(td3);
+
+      let td4 = document.createElement("td");
+      td4.innerText = av.City;
+      tr.appendChild(td4);
+
+      let td5 = document.createElement("td");
+      td5.innerText = av.State;
+      tr.appendChild(td5);
+
+      let td6 = document.createElement("td");
+      td6.innerText = av.ZipCode;
+      tr.appendChild(td6);
+
+      let td7 = document.createElement("td");
+      td7.innerText = av.Phone;
+      tr.appendChild(td7);
+
+      let td8 = document.createElement("td");
+      td8.innerText = av.Fax;
+      tr.appendChild(td8);
+
+      let td9 = document.createElement("td");
+      td9.textContent = av.Latitude;
+      tr.appendChild(td9);
+
+      let td10 = document.createElement("td");
+      td10.textContent = av.Longitude;
+      tr.appendChild(td10);
+
+      let td11 = document.createElement("td");
+      td11.textContent = `${av.Location.coordinates} ${av.Location.type}`;
+      tr.appendChild(td11);
+
+      let td13 = document.createElement("td");
+      td13.textContent = av.Visit;
+      tr.appendChild(td13);
+
+      tBodyEl.appendChild(tr);
+
+    }
+  });
+}
