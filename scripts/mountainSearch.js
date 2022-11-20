99d5cd8816f013467c89643d552f30e54ccd7df3
Sam Bhandari <sambhandari2021@yahooe.com>
2022-11-19 23:50:07 -0500

    able to add sunrise and sunset with calling APIi

--- scripts/mountainSearch.js
+++ scripts/mountainSearch.js
@@ -37,13 +37,23 @@ function displayItems() {
   outputimgDivEl.innerHTML = '';
   mountainsArray.forEach((av) => {
 
-    if (sMountainElValue == av.name) {
-      
+    if (sMountainElValue == av.name ) {
+      let sr = document.createElement('div');
+      console.log(sr);
+      outputimgDivEl.appendChild(sr);
+      sr.classList.add("mySr");
+
       let imgEl = document.createElement("img");
       outputimgDivEl.appendChild(imgEl);
       let imgSrc = av.img;
       imgEl.src = `../images/${imgSrc}`;
       imgEl.classList.add("myImg");
+
+
+      let ss = document.createElement('div');
+      console.log(ss);
+      outputimgDivEl.appendChild(ss);
+      ss.classList.add("mySs");
 
       let divName = document.createElement('div');
       divName.classList.add("myDiv");
@@ -62,6 +72,11 @@ function displayItems() {
       divLat.classList.add("myDiv")
       outputDivEl.appendChild(divLat);
       divLat.innerText = `Lat - ${av.coords.lat} / Lan - ${av.coords.lng}`;
+      getSunsetForMountain(av.coords.lat, av.coords.lng).then(data => {
+        console.log(data.results)
+        sr.innerText = `Sunrise ${data.results.sunrise} UTC`;
+        ss.innerText = `Sunset ${data.results.sunset} UTC`;
+      });
 
       let divDes = document.createElement("div");
       divDes.style = "font-size: large"
@@ -77,4 +92,60 @@ function displayItems() {
 
 
 }
+
+let lat = [];
+// console.log(lat)
+for (let i = 0; i < mountainsArray.length; i++) {
+  lat.push(mountainsArray[i].coords.lat);
+
+  
+}
+let lng = [];
+// console.log(lng)
+for (let i = 0; i < mountainsArray.length; i++) {
+  lng.push(mountainsArray[i].coords.lng);
+
+  
+}
+
+async function getSunsetForMountain(lat, lng){
+  let response = await fetch( `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`);
+     let data = await response.json();
+     return data;
+}
+
+
+
+// console.log(getSunsetForMountain());
+
+// Fetch the sunset/sunrise times for a specific mountain getSunsetForMountain(44.320686, -71.291742).then(data => {
+  // getSunsetForMountain(lat.forEach(arr => console.log(arr)),lng.forEach(arr => console.log(arr))).then(data => {
+  //   // lat.forEach(arr => console.log(arr) );
+    
+  //   console.log(data.results.sunset);
+  //   // let sunset = data.results.sunset;
+
+  //   // return sunset;
+
+  //   // return data.results.sunset
+  //   // let dataObject = data.results;
+  //   // console.log(dataObject.sunset);
+  // }).catch((message) => {
+  //   console.log('cannot find because ' + message);
+  // })
+
+  // console.log(getSunsetForMountain());
+
+  ///question
+// let lati = mountainsArray.forEach(function (arr) {
+//   return arr.coords.lat
+
+// });
+// console.log(lati);
+// console.log(lati);
+
+// let lng = mountainsArray.forEach((av) => {
+//   return (av.coords.lng);
+// });
+// console.log(lng);
 
